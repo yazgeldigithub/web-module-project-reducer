61d38a9467a7273242833897aeef8e2bd6ee32f3
yazgeldigithub <devster2016@gmail.com>
2021-06-07 16:09:45 -0500

    done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,38 @@
-export const ADD_ONE = "ADD_ONE";
+  
+export const ADD_ONE = 'ADD_ONE';
 
-export const APPLY_NUMBER = "APPLY_NUMBER";
-export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const APPLY_NUMBER = 'APPLY_NUMBER';
+export const CHANGE_OPERATION = 'CHANGE_OPERATION';
+export const CLEAR_TOTAL = 'CLEAR_TOTAL';
+
+export const SET_MEMORY = 'SET_MEMORY';
+export const RECALL_MEMORY = 'RECALL_MEMORY';
+export const CLEAR_MEMORY = 'CLEAR_MEMORY';
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+	return { type: ADD_ONE };
+};
 
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+export const applyNumber = number => {
+	return { type: APPLY_NUMBER, payload: number };
+};
+
+export const applyOperator = operator => {
+	return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearTotal = () => {
+	return { type: CLEAR_TOTAL };
+};
+
+export const setMemory = () => {
+	return { type: SET_MEMORY };
+};
+
+export const recallMemory = () => {
+	return { type: RECALL_MEMORY };
+};
+
+export const clearMemory = () => {
+	return { type: CLEAR_MEMORY };
+};
